1ce19e883b0a3a3bbeb989a7230951b93265ff9a
jCode319 <jdjack3@gmail.com>
2020-11-18 16:14:14 -0500

    Create a recipe route working

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -4,14 +4,35 @@ class RecipesController < ApplicationController
   #   require_login
   # end
 
-  get '/recipes' do
+  get '/recipes' do #read
     @recipes = Recipe.all
     @sorted_recipes = @recipes.sort_by {|r| r.name}
     erb :'/recipes/index'
   end
 
+  get '/recipes/new' do
+    erb :'recipes/new'
+  end
+
+  post '/recipes' do #create
+    binding.pry
+    @recipe = Recipe.new(params)
+
+
+  end
+
   get '/recipes/:id' do
-    erb :'/recipes/show'
+    @recipes = Recipe.find_by(id: params[:id])
+      # if @recipe
+        # @user = User.find(@recipe.user_id)
+        erb :'/recipes/show'
+      # else
+      #   redirect to '/recipes'
+      # end
+  end
+
+  get '/recipes/:id/edit' do
+    erb :'recipes/edit'
   end
 
 end
